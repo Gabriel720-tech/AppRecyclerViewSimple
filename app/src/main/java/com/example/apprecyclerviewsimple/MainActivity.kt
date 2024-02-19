aa251291b7bf7178ff4b7da97711ea39a7076f7b
Gabriel720-tech <angelorioscano64@gmail.com>
2024-02-19 14:24:41 -0500

    primer commit

---
+++ app/src/main/java/com/example/apprecyclerviewsimple/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.apprecyclerviewsimple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.apprecyclerviewsimple.databinding.ActivityMainBinding
+import com.example.apprecycleviewsimple.AdapterLista
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvlista.layoutManager = LinearLayoutManager(
+            applicationContext)
+        binding.rvlista.adapter = AdapterLista(listado())
+    }
+
+    private fun listado():List<String>{
+        val lista = ArrayList<String>()
+        var i = 0
+        while(i < 10){
+            lista.add("Valor $i")
+            i++
+        }
+        return lista
+    }
+}
